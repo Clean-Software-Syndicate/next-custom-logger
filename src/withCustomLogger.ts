54c73759ae09167493234a38c50f35b42b5dbe11
Zach Quintana <zach.quintana@gmail.com>
2021-09-29 11:06:05 -0600

    Initial commit

---
+++ src/withCustomLogger.ts
@@ -0,0 +1,49 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { ParsedUrlQuery } from 'querystring'
+import Error from "next/error";
+
+interface NextServer {
+    getServerRequestHandler(): any
+    server: any
+}
+
+type RequestContext = {
+    req: IncomingMessage
+    res: ServerResponse
+    pathname: string
+    query: ParsedUrlQuery
+}
+
+interface LoggerMethod {
+    (ctx: RequestContext, err: Error | null): void
+}
+
+interface RenderErrorToResponse {
+    (ctx: RequestContext, _err: Error | null): Promise<any>
+}
+
+const wrapRenderToErrorResponse = (originalRenderer: RenderErrorToResponse) => (loggerMethod: LoggerMethod) => {
+    return async function (this: NextServer, ctx: RequestContext, err: Error | null) {
+        loggerMethod.call(this, ctx, err);
+
+        return originalRenderer.call(this, ctx, err);
+    }
+}
+
+const wrapServerRequestHandler = (server: NextServer, loggerMethod: LoggerMethod) => {
+    const originalHandler = server.getServerRequestHandler;
+
+    return async function(this: NextServer) {
+        const serverProto = Object.getPrototypeOf(this.server);
+        serverProto.renderErrorToResponse = wrapRenderToErrorResponse(serverProto.renderErrorToResponse)(loggerMethod);
+
+        return originalHandler.call(this);
+    }
+}
+
+export default function withCustomLogger(server: NextServer, loggerMethod: LoggerMethod) {
+    const serverProto = Object.getPrototypeOf(server);
+    serverProto.getServerRequestHandler = wrapServerRequestHandler(server, loggerMethod);
+
+    return server;
+}
